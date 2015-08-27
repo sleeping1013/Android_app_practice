c229483b330a6207a62982e34df212af1a4364cf
sleeping1013 <sleeping1013@gmail.com>
2015-08-27 22:13:08 +0800

    FirstClass
    It's fucking hard

---
+++ app/src/main/java/com/lucas/sampleui/MainActivity.java
@@ -0,0 +1,87 @@
+package com.lucas.sampleui;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Vector;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private EditText inputText;
+    private CheckBox hide;
+
+    private SharedPreferences sp;
+    private SharedPreferences.Editor editor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sp = getSharedPreferences("settings", Context.MODE_PRIVATE);
+        editor = sp.edit();
+
+        inputText = (EditText) findViewById(R.id.editText);
+        //inputText.setText("Hello World");
+        inputText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                editor.putString("input", inputText.getText().toString());
+                editor.commit();
+
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    submit(v);
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+        inputText.setText(sp.getString("input", ""));
+        hide = (CheckBox) findViewById(R.id.checkBox); // 在這裡才能開始拿到實體
+
+    }
+    public void submit (View view) {
+        String text = inputText.getText().toString();
+        if (hide.isChecked()) {
+            text = "*********";
+        }
+        inputText.setText("");
+        Toast.makeText(this,text,Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
