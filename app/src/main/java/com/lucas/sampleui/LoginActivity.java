136553d207d0c1d15065600c80380350a5705d32
sleeping1013 <sleeping1013@gmail.com>
2015-09-24 22:25:21 +0800

    layout and init configuration
    see the btn

--- app/src/main/java/com/lucas/sampleui/LoginActivity.java
+++ app/src/main/java/com/lucas/sampleui/LoginActivity.java
@@ -1,11 +1,19 @@
 package com.lucas.sampleui;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.facebook.FacebookSdk;
 
 /**
  * Created by user on 2015/9/24.
  */
 public class LoginActivity extends Activity{
 
-
+    @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                FacebookSdk.sdkInitialize(getApplicationContext());
+                super.onCreate(savedInstanceState);
+               setContentView(R.layout.activity_login);
+            }
 }
